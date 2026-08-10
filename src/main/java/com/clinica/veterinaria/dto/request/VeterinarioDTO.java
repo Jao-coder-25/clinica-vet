b64f34129fdc09bc4a65a9c419eb2fd286026d90
João Luiz <amorimsantosjoaoluiz@gmail.com>
2026-08-10 19:44:16 -0300

    feat: adiciona pakcage request para fazer sepração de camadas

---
+++ src/main/java/com/clinica/veterinaria/dto/request/VeterinarioDTO.java
@@ -0,0 +1,23 @@
+package com.clinica.veterinaria.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.validation.constraints.NotBlank;
+
+public record VeterinarioDTO(
+
+        @NotBlank(message = "A especialidade do veterinário é obrigatório.")
+        @JsonAlias({"especialidade_veterinario"})
+        String especialidadeVeterinario,
+
+        @NotBlank(message = "O nome do veterinário é obrigatório.")
+        @JsonAlias({"nome_veterinario"})
+        String nomeVeterinario,
+
+        @NotBlank(message = "O telefone do veterinário é obrigatório.")
+        @JsonAlias({"telefone_veterinario"})
+        String telefoneVeterinario,
+
+        @NotBlank(message = "O CPF é obrigatório")
+        @JsonAlias({"cpf_veterinario"})
+        String cpfVeterinario
+) {}
