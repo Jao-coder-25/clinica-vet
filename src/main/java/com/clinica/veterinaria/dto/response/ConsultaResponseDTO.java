f704c682dc542c411f1c9c2ced1325ff71eae2b3
João Luiz <amorimsantosjoaoluiz@gmail.com>
2026-08-11 20:22:51 -0300

    docs(consulta): adiciona comentario explicativo sobre o funcionamento do construtor no DTO

--- src/main/java/com/clinica/veterinaria/dto/response/ConsultaResponseDTO.java
+++ src/main/java/com/clinica/veterinaria/dto/response/ConsultaResponseDTO.java
@@ -12,6 +12,9 @@ public record ConsultaResponseDTO(
         Long idVeterinario,
         String nomeVeterinario
 ) {
+    // CONSTRUTOR PERSONALIZADO (O Java identifica como construtor porque tem o mesmo nome do arquivo/Record e não possui tipo de retorno).
+    // Ele funciona como um tradutor automático: extrai apenas os campos necessários da entidade do banco de dados,
+    // injeta nas variáveis do Record (respeitando estritamente a ordem declarada no topo) e descarta os dados internos redundantes
     public ConsultaResponseDTO(ConsultaEntity consulta) {
         this(
                 consulta.getIdConsulta(),
