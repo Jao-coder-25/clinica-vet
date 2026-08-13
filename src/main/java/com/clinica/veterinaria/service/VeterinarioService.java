8d1c67d243575830e7728e62e248caa1f8f70e97
João Luiz <amorimsantosjoaoluiz@gmail.com>
2026-08-12 21:29:18 -0300

    feat(veterinario): adiciona regra de negocio para deleção segura no VeterinarioService

--- src/main/java/com/clinica/veterinaria/service/VeterinarioService.java
+++ src/main/java/com/clinica/veterinaria/service/VeterinarioService.java
@@ -3,16 +3,20 @@ package com.clinica.veterinaria.service;
 import com.clinica.veterinaria.dto.request.VeterinarioDTO;
 import com.clinica.veterinaria.dto.response.VeterinarioResponseDTO;
 import com.clinica.veterinaria.entity.VeterinarioEntity;
+import com.clinica.veterinaria.repository.ConsultaRepository;
 import com.clinica.veterinaria.repository.VeterinarioRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VeterinarioService {
 
     private final VeterinarioRepository veterinarioRepository;
+    private final ConsultaRepository consultaRepository;
 
-    public VeterinarioService(VeterinarioRepository veterinarioRepository) {
+    public VeterinarioService(VeterinarioRepository veterinarioRepository, ConsultaRepository consultaRepository) {
         this.veterinarioRepository = veterinarioRepository;
+        this.consultaRepository = consultaRepository;
     }
     public VeterinarioResponseDTO save(VeterinarioDTO veterinarioDTO){
         boolean cpfExistente = veterinarioRepository.existsByCpfVeterinario(veterinarioDTO.cpfVeterinario());
@@ -28,5 +32,17 @@ public class VeterinarioService {
                 .build();
 
         return new VeterinarioResponseDTO(veterinarioRepository.save(veterinarioEntity));
+    }
+    public void delete(Long idVeterinario) {
+
+        if(!veterinarioRepository.existsById(idVeterinario)){
+            throw new IllegalArgumentException("O veterinário com o ID informado não existe.");
+        }
+        boolean possuiConsultas = consultaRepository.existsByVeterinarioIdVeterinario(idVeterinario);
+
+        if (possuiConsultas) {
+            throw new IllegalArgumentException("Não é possível excluir o veterinário, pois ele possui consultas associadas.");
+        }
+        veterinarioRepository.deleteById(idVeterinario);
     }
 }
