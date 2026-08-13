f591b6e95b02fc7334baff5d2e4b01d810b2891f
João Luiz <amorimsantosjoaoluiz@gmail.com>
2026-08-13 19:59:22 -0300

    refactor(consulta): remove anotacao Repository redundante do ConsultaRepository

--- src/main/java/com/clinica/veterinaria/repository/ConsultaRepository.java
+++ src/main/java/com/clinica/veterinaria/repository/ConsultaRepository.java
@@ -2,9 +2,7 @@ package com.clinica.veterinaria.repository;
 
 import com.clinica.veterinaria.entity.ConsultaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ConsultaRepository extends JpaRepository<ConsultaEntity, Long> {
 
     boolean existsByVeterinarioIdVeterinario(Long idVeterinario);
