ce64785237cbd0eb6775d8fe2ee1bb9829afc1b8
João Luiz <amorimsantosjoaoluiz@gmail.com>
2026-08-12 21:29:49 -0300

    feat(veterinario): expoe endpoint DeleteMapping corrigido no VeterinarioController

--- src/main/java/com/clinica/veterinaria/controller/VeterinarioController.java
+++ src/main/java/com/clinica/veterinaria/controller/VeterinarioController.java
@@ -2,15 +2,11 @@ package com.clinica.veterinaria.controller;
 
 import com.clinica.veterinaria.dto.request.VeterinarioDTO;
 import com.clinica.veterinaria.dto.response.VeterinarioResponseDTO;
-import com.clinica.veterinaria.entity.VeterinarioEntity;
 import com.clinica.veterinaria.service.VeterinarioService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/veterinarios")
@@ -26,5 +22,10 @@ public class VeterinarioController {
     public ResponseEntity<VeterinarioResponseDTO> save(@RequestBody @Valid VeterinarioDTO veterinarioDTO) {
         VeterinarioResponseDTO salvarVeterinario = veterinarioService.save(veterinarioDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(salvarVeterinario);
+    }
+    @DeleteMapping("/{idVeterinario}")
+    public ResponseEntity<Void> delete(@PathVariable Long idVeterinario) {
+        veterinarioService.delete(idVeterinario);
+        return ResponseEntity.noContent().build();
     }
 }
