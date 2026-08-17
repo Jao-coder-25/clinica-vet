f7ee086a6f3197118a2d62395cbb129aeae4897c
João Luiz <amorimsantosjoaoluiz@gmail.com>
2026-08-17 20:34:48 -0300

    fix: removendo import não utilizado

--- src/main/java/com/clinica/veterinaria/repository/ConsultaRepository.java
+++ src/main/java/com/clinica/veterinaria/repository/ConsultaRepository.java
@@ -2,10 +2,8 @@ package com.clinica.veterinaria.repository;
 
 import com.clinica.veterinaria.entity.ConsultaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.List;
 
 public interface ConsultaRepository extends JpaRepository<ConsultaEntity, Long> {
 
