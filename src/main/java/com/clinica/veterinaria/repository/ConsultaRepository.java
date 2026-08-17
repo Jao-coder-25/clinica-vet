401fcafc681c9766655ec40ee0ef54063dbac403
João Luiz <amorimsantosjoaoluiz@gmail.com>
2026-08-17 20:09:51 -0300

    feat: adiciona assinatura de busca por data no ConsultaRepository

--- src/main/java/com/clinica/veterinaria/repository/ConsultaRepository.java
+++ src/main/java/com/clinica/veterinaria/repository/ConsultaRepository.java
@@ -3,9 +3,12 @@ package com.clinica.veterinaria.repository;
 import com.clinica.veterinaria.entity.ConsultaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ConsultaRepository extends JpaRepository<ConsultaEntity, Long> {
 
     boolean existsByVeterinarioIdVeterinario(Long idVeterinario);
     boolean existsByPetIdPet(Long idPet);
-
+    List<ConsultaEntity> findAllByDataConsulta(LocalDate dataConsulta);
 }
