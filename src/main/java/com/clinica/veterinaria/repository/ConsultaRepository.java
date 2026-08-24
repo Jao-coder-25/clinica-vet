9ae3a830185c18953fb9376a42347ca46c9434dd
João Luiz <amorimsantosjoaoluiz@gmail.com>
2026-08-24 20:18:27 -0300

    fix: adiciona @Param no metodo findHorariosOcupadosPorData

--- src/main/java/com/clinica/veterinaria/repository/ConsultaRepository.java
+++ src/main/java/com/clinica/veterinaria/repository/ConsultaRepository.java
@@ -1,9 +1,10 @@
 package com.clinica.veterinaria.repository;
 
-import com.clinica.veterinaria.dto.response.ConsultaResponseDTO;
 import com.clinica.veterinaria.entity.ConsultaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
@@ -13,6 +14,6 @@ public interface ConsultaRepository extends JpaRepository<ConsultaEntity, Long> 
     boolean existsByVeterinarioIdVeterinario(Long idVeterinario);
     boolean existsByPetIdPet(Long idPet);
     @Query("SELECT c.horarioConsulta FROM ConsultaEntity c WHERE c.dataConsulta = :dataConsulta")
-    List<LocalTime> findHorariosOcupadosPorData(LocalDate dataConsulta);
+    List<LocalTime> findHorariosOcupadosPorData(@Param("dataConsulta") LocalDate dataConsulta);
     List<ConsultaEntity> findAllById(Long idConsulta);
 }
